e7cae783ec2df0b8ff6241f1b17e84773a1b37d5
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-01 11:27:29 +0200

    feat(storefront): generate metadata for pages

--- apps/storefront/src/app/(with-navigation)/products/[productId]/page.tsx
+++ apps/storefront/src/app/(with-navigation)/products/[productId]/page.tsx
@@ -1,3 +1,20 @@
+import type { Metadata } from 'next';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { productId: string };
+}): Promise<Metadata> {
+  const res = await fetch(
+    'https://naszsklep-api.vercel.app/api/products/' + params.productId
+  );
+  const product = (await res.json()) as { title: string; description: string };
+
+  return {
+    title: product.title,
+  };
+}
+
 export async function generateStaticParams() {
   const res = await fetch(`https://naszsklep-api.vercel.app/api/products`);
   const products = (await res.json()) as { id: string; title: string }[];
