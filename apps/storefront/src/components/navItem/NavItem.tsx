208b83d1674636ca6ab2fb228419db7716f150df
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-02 01:09:13 +0200

    feat(storefront): add active state to nav link

--- apps/storefront/src/components/navItem/NavItem.tsx
+++ apps/storefront/src/components/navItem/NavItem.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Route } from 'next';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface NavItemProps<T extends string> {
   url: Route<T>;
@@ -10,9 +13,17 @@ export const NavItem = <T extends string>({
   url,
   children,
 }: NavItemProps<T>) => {
+  const pathname = usePathname();
+
   return (
-    <li className="block text-white font-bold bg-blue-700 hover:bg-blue-600 active:bg-blue-800 px-4 py-2">
-      <Link href={url}>{children}</Link>
+    <li
+      className={`block ${
+        pathname === url ? 'text-red-600' : 'text-white'
+      } font-bold bg-blue-700 hover:bg-blue-600 active:bg-blue-800 px-4 py-2`}
+    >
+      <Link href={url} aria-current={pathname === url}>
+        {children}
+      </Link>
     </li>
   );
 };
