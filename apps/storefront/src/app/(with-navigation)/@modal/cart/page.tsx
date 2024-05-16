39b8d4f52e63b8bcc37266f8a03eb47115ffac83
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-16 12:43:50 +0200

    feat(cart): add modal as parallel route

---
+++ apps/storefront/src/app/(with-navigation)/@modal/cart/page.tsx
@@ -0,0 +1,12 @@
+export default function CartModalPage() {
+  return (
+    <div className="absolute right-0 z-50 h-screen w-full max-w-sm bg-gray-300 opacity-70 text-black p-8">
+      <h2 className="text-3xl text-black p-8">Cart</h2>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  );
+}
