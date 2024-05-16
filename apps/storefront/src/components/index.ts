e02ac3e2c360d5d720ff2096ac994bf6adcbf8a7
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-16 13:15:56 +0200

    feat(cart): use intercepting route to display cart content

--- apps/storefront/src/components/index.ts
+++ apps/storefront/src/components/index.ts
@@ -3,3 +3,6 @@ export * from './menu';
 export * from './navigation';
 export * from './navItem';
 export * from './productListItem';
+export * from './overlay';
+export * from './coverImage';
+export * from './dogListItem';
